61a2167317c45015999966ddcda9abf7aaaeb7d0
tohidi <s.tohidi22@gmail.com>
2024-04-18 23:03:13 +0330

    first file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from './Hello';
 
 function App() {
+  const users = [
+    {
+      firstname: "saeide",
+    },
+    {
+      firstname: "shima",
+    },
+    {
+      firstname: "sara",
+    },
+    {
+      firstname: "samane",
+    }
+
+  ]
+  const user = "Sara"
+
+  const elementName = <h1>{users[0].firstname}</h1>
+
   return (
     <div className="App">
       <header className="App-header">
+
+        {/* hello */}
         <img src={logo} className="App-logo" alt="logo" />
+        <Hello />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {elementName}
+        <p>
+          ________
+        </p>
+        {user ? <h1>hello {user}</h1> : <h1>hello user</h1>}
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,7 +45,15 @@ function App() {
         >
           Learn React
         </a>
+        <br />
+        {
+          users.map(item => (
+            <h5>{item.firstname}</h5>
+
+          ))}
+
       </header>
+
     </div>
   );
 }
